eeff84e235d180d60db4bc6a96c6fdf0bbf762d7
BlueHowl <avert45officiel@gmail.com>
2024-03-16 00:51:20 +0100

    Added capacitor (multi-platform)

---
+++ android/app/src/main/java/be/city/conquest/MainActivity.java
@@ -0,0 +1,5 @@
+package be.city.conquest;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'be.city.conquest',
+  appName: 'CityConquest',
+  webDir: 'dist/web-angular-ctofwal',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
