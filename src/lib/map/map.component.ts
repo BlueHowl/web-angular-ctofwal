8c6f0541bd0f992ae4fd20641801586f0489fd37
BlueHowl <avert45officiel@gmail.com>
2024-03-16 01:44:28 +0100

    First component

---
+++ src/lib/map/map.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'city-map',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+
+export class CityMapComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
+
