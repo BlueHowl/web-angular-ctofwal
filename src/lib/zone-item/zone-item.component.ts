d29e16b51216b4f7c407202ee568c769361f1629
Sougne Guillaume <guillaume@sougne.be>
2024-03-16 13:11:19 +0100

    modal

--- src/lib/zone-item/zone-item.component.ts
+++ src/lib/zone-item/zone-item.component.ts
@@ -7,18 +7,13 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 })
 export class ZoneItemComponent {
   isBlackAndWhite: boolean = false;
-  isTextVisible: boolean = false;
+  isModalVisible: boolean = false;
 
   handleGetDetails() {
-    this.isTextVisible = !this.isTextVisible; // Show text when image is clicked
+    this.isModalVisible = !this.isModalVisible; // Show text when image is clicked
     console.log('details')
-    console.log('isVisible' + this.isTextVisible)
+    console.log('isVisible' + this.isModalVisible)
     console.log('is B&W' + this.isBlackAndWhite)
-  }
-
-  hideText() {
-    this.isTextVisible = false; // Hide text when clicking elsewhere
-    console.log("hide")
   }
 
   @Input() xPosition: number = 0;
