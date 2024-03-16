2c13247fd4b9436963ae24767608883f86e01b86
BlueHowl <avert45officiel@gmail.com>
2024-03-16 10:14:11 +0100

    WIP PinPoint Display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,26 @@ export class AppComponent {
 
   caroloMap = {
     img: 'map-charleroi.png',
-    topLeftCorner: { lat: 0, lng: 0 },
-    bottomRightCorner: { lat: 0, lng: 0 },
+    topLeftCorner: { lat: 50.50118509830947, lng: 4.345420925564241 },
+    bottomRightCorner: { lat: 50.35616625708724, lng: 4.51919233768808 },
     pois: [
       {
         id: 0,
-        name: 'test',
-        img: 'test.png',
-        coordinate: { lat: 1000, lng: 200 }
+        name: 'Hotel de ville de charleroi',
+        img: 'hotel.png',
+        coordinate: { lat: 50.41192225838149, lng: 4.444018139827997 }
       },
       {
         id: 1,
-        name: 'test2',
-        img: 'test2.png',
-        coordinate: { lat: 100, lng: 200 }
+        name: 'A6K - Charleroi',
+        img: 'a6k.png',
+        coordinate: { lat: 50.40508061318812, lng: 4.439906300111793 }
       },
       {
-        id: 2,
-        name: 'test3',
-        img: 'test3.png',
-        coordinate: { lat: 600, lng: 800 }
+        id: 3,
+        name: 'test outisde map',
+        img: 'testO.png',
+        coordinate: { lat: 50.364209, lng: 4.546653 }
       }
     ]
   }
