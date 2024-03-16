439df3d49d839af0eb33a4fec136a97615ac1505
BlueHowl <avert45officiel@gmail.com>
2024-03-16 20:42:14 +0100

    .

--- src/lib/zone-item/zone-item.component.ts
+++ src/lib/zone-item/zone-item.component.ts
@@ -36,12 +36,12 @@ OnConfirmChallenge() {
   constructor() { }
   ngOnInit(): void {
 
-    this.pinImg.src = 'assets/img/poi/' + this.point?.img || '';
+    this.pinImg.src = 'assets/img/pinpoint.png'; //poi/' + this.point?.img || '';
 
     this.pinImg.onload = () => {
       //Centre le point sur le milieu de l'image
       this.centerdX = this.point.position.x - (50/2) ; //TODO changed 100 to img width in view
-      this.centerdY = this.point.position.y - (50/2); //TODO changed 100 to img height in view
+      this.centerdY = this.point.position.y - (50/2) - 20; //TODO changed 100 to img height in view
     };
     this.HeaderContent = this.content?.HeaderContent ||"Unlocked"
     this.HeaderImg = this.content?.HeaderImg||""
